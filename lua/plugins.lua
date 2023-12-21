4ad8dcc507ba3339709c6208f6423e607e1f88eb
Bassam Safadi <samsafadi@gmail.com>
2023-12-21 14:54:14 -0500

    couple changes

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -120,7 +120,7 @@ return {
     opts = {
       options = {
         icons_enabled = true,
-        theme = 'tokyonight',
+        theme = 'auto',
       component_separators = { left = '|', right = '|'},
       section_separators = { left = '', right = ''},
       },
