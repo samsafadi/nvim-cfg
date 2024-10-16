a46b382c64fc83ed4a478356e1b3ac2845128819
Bassam Safadi <samsafadi@gmail.com>
2024-10-16 10:21:52 -0400

    fix python path issues

--- lua/config/globals.lua
+++ lua/config/globals.lua
@@ -4,14 +4,10 @@ local host_handle = io.popen('hostname')
 if host_handle ~= nil then
   local hostname = host_handle:read('*a')
   hostname = string.gsub(hostname, "\n$", "")
-  if hostname == 'C02C2107MD6V' or 'JVGHGJ0H40' then
+  if hostname == 'C02C2107MD6V' or hostname == 'JVGHGJ0H40' then
     PYTHON_PATH = '/Users/bassamsafadi/work/data/job-controller/.tox/py39/bin/python3.9'
   else
-    local pypath_handle = io.popen('which python3')
-    if pypath_handle ~= nil then
-      PYTHON_PATH = pypath_handle:read("*a")
-      pypath_handle:close()
-    end
+    PYTHON_PATH = vim.fn.exepath("python")
   end
   host_handle:close()
 end
