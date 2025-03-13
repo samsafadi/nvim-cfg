132ccd1901ea81cd80b6be39fb749959b53b7986
Bassam Safadi <samsafadi@gmail.com>
2025-03-13 09:24:04 -0400

    noice recording message

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -310,6 +310,12 @@ return {
         inc_rename = false, -- enables an input dialog for inc-rename.nvim
         lsp_doc_border = false, -- add a border to hover docs and signature help
       },
+      routes = {
+        {
+          view = "notify",
+          filter = { event = "msg_showmode" },
+        },
+      },
     },
     dependencies = {
       -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
